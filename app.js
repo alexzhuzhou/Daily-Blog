7b6719e2680f3aa0319bccc4ca76b843f9d49928
Tian Hong Zhu Zhou <alexzhuzhou@gmail.com>
2025-01-09 19:27:36 -0800

    Add a Procfile

--- app.js
+++ app.js
@@ -273,8 +273,10 @@ app.get("/logout", function(req,res) {
   }); 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
